79394eb09b0ed9601f45a6351492a7de19466fc0
zaifeng <zaifeng@users.noreply.github.com>
2023-05-27 15:04:42 +0000

    deploy: zaifeng/blog@1a0a820de1c0d001947373f15b4dd00ff2762c4b

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,10 @@ $("#rocket").click(function() {
     });
     return false;
 });
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?22fc6aa0ad1f27f688892fdf22d183fa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
